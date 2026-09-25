aafcca32df6d8e513a00ff41eb5f7d406e701286
ilyas8585 <ilyas_unusbaev@mail.ru>
2026-09-25 15:58:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -154,6 +154,12 @@ def telegram_videos():
 
                         if not message.video:
                             continue
+                            # Пропускаем длинные и тяжёлые видео
+                            if message.video.duration and message.video.duration > 45:
+                                continue
+                            
+                            if message.file and message.file.size and message.file.size > 25 * 1024 * 1024:
+                                continue
                             key = f"{channel_name}:{message.id}"
 
                             if key in published:
